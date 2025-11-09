7e2e02259ced2f5b56a356685aaa89ade7c73ba2
brownae1331 <brownae1331@gmail.com>
2025-11-09 17:07:01 +0000

    Climbing Stairs

---
+++ 1-D Dynamic Programming/Easy/Climbing_Stairs.py
@@ -0,0 +1,11 @@
+def climbStairs(n: int) -> int:
+    one, two = 1, 1
+
+    for _ in range(n-1):
+        temp = one
+        one = one + two
+        two = temp
+    return one 
+
+if __name__ == "__main__":
+    print(climbStairs(n=5))
