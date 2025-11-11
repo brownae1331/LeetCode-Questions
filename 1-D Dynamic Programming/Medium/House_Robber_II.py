58a5a9c8b38277f5bdd47b219963dbf3e8c927ee
brownae1331 <brownae1331@gmail.com>
2025-11-11 12:37:44 +0000

    House Robber II

---
+++ 1-D Dynamic Programming/Medium/House_Robber_II.py
@@ -0,0 +1,22 @@
+def rob(nums: list[int]) -> int:
+    def helper(nums: list[int]) -> int:
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i - 1], nums[i] + dp[i - 2])
+
+        return dp[-1]
+    
+    if len(nums) == 1:
+        return nums[0]
+    return max(helper(nums[1:]), helper(nums[:-1]))
+
+if __name__ == "__main__":
+    print(rob(nums=[50,9,8,40,6]))
